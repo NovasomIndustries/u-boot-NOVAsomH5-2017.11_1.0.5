d6930e56509ad819a048a24efca0383f4459b8db
fil <fil@localhost.localdomain>
2018-02-19 17:03:28 +0100

    Added bl31.bin, remove old H5 output directory

--- H5/include/generated/version_autogenerated.h
+++
@@ -1,4 +0,0 @@
-#define PLAIN_VERSION "2017.11"
-#define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CC_VERSION_STRING "aarch64-buildroot-linux-gnu-gcc.br_real (Buildroot 2017.11) 6.4.0"
-#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.28.1"
